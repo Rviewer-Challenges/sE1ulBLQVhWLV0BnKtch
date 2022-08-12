2a3087a73eb75b0a00eacd8804d77d0c858de14b
MrKevin574 <mrkevin574@gmail.com>
2022-08-12 15:58:31 -0400

    first commit

---
+++ app/src/main/java/com/mrkevin574/chatfirebase/ui/theme/Theme.kt
@@ -0,0 +1,20 @@
+package com.mrkevin574.chatfirebase.ui.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ChatFirebaseTheme(content: @Composable () -> Unit) {
+    val colors = darkColors(
+        primary = PrimaryColor,
+        primaryVariant = PrimaryLightColor,
+        secondary = SecondaryColor)
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
