79595f0064cab7731740a9b876756068dd5f2ee1
MrKevin574 <mrkevin574@gmail.com>
2022-08-12 16:25:57 -0400

    Finalizing Splash Screen

--- app/src/main/java/com/mrkevin574/chatfirebase/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/mrkevin574/chatfirebase/ui/screens/SplashScreen.kt
@@ -1,9 +1,35 @@
 package com.mrkevin574.chatfirebase.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+import com.google.firebase.auth.FirebaseAuth
+import com.mrkevin574.chatfirebase.R
+import com.mrkevin574.chatfirebase.ui.theme.Screens
 
 @Composable
 fun SplashScreen(navController: NavController) {
+    ContentSplashScreen()
+    val auth = FirebaseAuth.getInstance()
+    if(auth.currentUser == null) navController.navigate(Screens.LoginScreen.route)
+    else navController.navigate(Screens.MainScreen.route)
+}
 
+@Composable
+fun ContentSplashScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    )
+    {
+        Image(painter = painterResource(id = R.drawable.firebase_logo), contentDescription = stringResource(
+            id = R.string.firebase_logo
+        ))
+    }
 }
