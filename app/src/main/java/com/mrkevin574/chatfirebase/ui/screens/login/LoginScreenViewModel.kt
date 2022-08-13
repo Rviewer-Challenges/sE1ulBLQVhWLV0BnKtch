003c0653fd2b98588787188677fde9226f9b358d
MrKevin574 <mrkevin574@gmail.com>
2022-08-13 15:31:37 -0400

    Cleaning code

--- app/src/main/java/com/mrkevin574/chatfirebase/ui/screens/login/LoginScreenViewModel.kt
+++ app/src/main/java/com/mrkevin574/chatfirebase/ui/screens/login/LoginScreenViewModel.kt
@@ -4,7 +4,6 @@ import android.content.IntentSender
 import androidx.activity.result.ActivityResult
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
-import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.mrkevin574.chatfirebase.ui.data.GoogleSignInService
 import com.mrkevin574.chatfirebase.ui.theme.Screens
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -15,17 +14,13 @@ class LoginScreenViewModel @Inject constructor(
     private val service : GoogleSignInService
 ) : ViewModel() {
 
-    fun getIntentSender( intentSender : (IntentSender) -> Unit)
-    {
-        service.getIntentSender(launchRequest = intentSender)
-    }
+    fun getIntentSender( intentSender : (IntentSender) -> Unit) = service.getIntentSender(launchRequest = intentSender)
 
     fun onResult(result : ActivityResult, navController: NavController)
     {
         result.data?.let { intent ->
             service.signInWithInFirebase(intent){ resp ->
                 if(resp) navController.navigate(Screens.MainScreen.route)
-                else ""
             }
         }
     }
