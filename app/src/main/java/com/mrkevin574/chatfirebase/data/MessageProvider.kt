9a7008ac6e1e77dab2a7213f0de2296541e88f12
MrKevin574 <mrkevin574@gmail.com>
2022-08-15 09:04:43 -0400

    Adding method to generate uniqueId for conversations

--- app/src/main/java/com/mrkevin574/chatfirebase/data/MessageProvider.kt
+++ app/src/main/java/com/mrkevin574/chatfirebase/data/MessageProvider.kt
@@ -1,11 +1,36 @@
 package com.mrkevin574.chatfirebase.data
 
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.mrkevin574.chatfirebase.data.model.Message
+import com.mrkevin574.chatfirebase.util.CONST_MESSAGES
+import com.mrkevin574.chatfirebase.util.USER_COLLECTION
 import javax.inject.Inject
 
-class MessageProvider @Inject constructor() {
-    fun getMessagesByIdForCurrentUser(localUserId : String, userReceiverId : String) : List<Message>
+class MessageProvider @Inject constructor(
+    private val db : FirebaseDatabase
+) {
+    fun getMessagesByIdForCurrentUser(localUserId : String, userReceiverId : String, callback : (List<Message>) -> Unit)
     {
-        return emptyList()
+        val ref = db.getReference(getUniqueIdConversation(localUserId, userReceiverId))
+        ref.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+              //  callback()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
+
+    private fun getUniqueIdConversation(userId1 : String, userId2 : String) : String
+    {
+        val usersIdList = listOf(userId1, userId2).sorted()
+        return "${usersIdList[0]}_${usersIdList[0]}"
+    }
+
 }
