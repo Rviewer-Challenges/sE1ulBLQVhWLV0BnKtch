0de77610e8a3cb9bea373cabbb46f8ad8eab23b4
MrKevin574 <mrkevin574@gmail.com>
2022-08-17 13:51:21 -0400

    Implementation Repository

--- app/src/main/java/com/mrkevin574/chatfirebase/data/model/UsersResponse.kt
+++ app/src/main/java/com/mrkevin574/chatfirebase/data/model/UsersResponse.kt
@@ -1,3 +1,3 @@
 package com.mrkevin574.chatfirebase.data.model
 
-data class UsersResponse(val success : Boolean, val userList : List<User>)
+data class UsersResponse(val success : Boolean, var userList : List<User>)
