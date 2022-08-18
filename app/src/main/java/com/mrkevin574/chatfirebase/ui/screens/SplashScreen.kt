8a189ef7cb745da54fedcec2cff8b1296a1fdeee
MrKevin574 <mrkevin574@gmail.com>
2022-08-18 19:46:07 -0400

    Finalizing DataStore

--- app/src/main/java/com/mrkevin574/chatfirebase/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/mrkevin574/chatfirebase/ui/screens/SplashScreen.kt
@@ -5,21 +5,45 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.google.firebase.auth.FirebaseAuth
 import com.mrkevin574.chatfirebase.R
+import java.util.*
 
 @Composable
-fun SplashScreen(navController: NavController) {
+fun SplashScreen(
+    navController: NavController,
+    viewModel: SplashViewModel = hiltViewModel()
+) {
     ContentSplashScreen()
-    val auth = FirebaseAuth.getInstance()
-    val value = auth.currentUser == null
-    if(value)  LaunchedEffect(value){navController.navigate(Screens.LoginScreen.route)}
-    else LaunchedEffect(value){navController.navigate(Screens.MainScreen.route)}
+    LaunchedEffect(key1 = "1"){
+        viewModel.getSession().collect {
+            if(it != null)
+            {
+                if(it)
+                {
+                    navController.navigate(Screens.MainScreen.route)
+                }
+            }else{
+                val auth = FirebaseAuth.getInstance()
+                val value = auth.currentUser == null
+                if(value)  navController.navigate(Screens.LoginScreen.route)
+                else {
+                    navController.navigate(Screens.MainScreen.route)
+                    viewModel.saveSession(true)
+                }
+            }
+        }
+    }
+
+
+
 }
 
 @Composable
